84bd3c0064f135a991a2c0ec8a8156b0e4b891e1
William Budington <bill.budington@gmail.com>
2011-11-17 20:19:47 -0800

    Removing all references to Job Board

--- config-example.js
+++ config-example.js
@@ -7,7 +7,7 @@ module.exports = {
   },
   host: 'localhost',
   port: 8001,
-  failed_restart_subject: 'Critical: Job Board failing on restart',
+  failed_restart_subject: 'Critical: Child process failing on restart',
   failed_restart_body: 'Child process is failing on restart.  Your attention is required to fix the problem',
   max_restarts_subject: 'Warning: Max restarts reached',
   max_restarts_body: 'Child process restarts have reached the critical threshold.  You may want to check up on the child process.'
